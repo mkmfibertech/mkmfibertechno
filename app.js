1657c2ee984daeb813e73041e6afcfeb08983898
mkmfibertech <mkm.fibertech@gmail.com>
2026-06-07 21:30:31 +0300

    Implement app installation prompt in service worker
    زر تثبيت التطبيق PWA

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 self.addEventListener("install", e => {
   console.log("Service Worker Installed");
 });
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+
+    e.preventDefault();
+
+    deferredPrompt = e;
+
+    const installBtn = document.createElement('button');
+
+    installBtn.innerText = '📱 تثبيت التطبيق';
+
+    installBtn.className = 'btn';
+
+    document.body.prepend(installBtn);
+
+    installBtn.addEventListener('click', () => {
+
+        deferredPrompt.prompt();
+
+        deferredPrompt.userChoice.then(() => {
+
+            installBtn.remove();
+
+        });
+
+    });
+
+});
