6392f489b4d9c2fb51b21c7b12d953acb3a6d21c
mkmfibertech <mkm.fibertech@gmail.com>
2026-06-08 14:54:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -29,5 +29,26 @@ window.addEventListener('beforeinstallprompt', (e) => {
         });
 
     });
+  
 
 });
+
+
+const slides = document.querySelectorAll('.slide');
+
+let currentSlide = 0;
+
+function showSlide() {
+
+    slides[currentSlide]
+        .classList.remove('active');
+
+    currentSlide =
+        (currentSlide + 1) % slides.length;
+
+    slides[currentSlide]
+        .classList.add('active');
+
+}
+
+setInterval(showSlide, 5000);
