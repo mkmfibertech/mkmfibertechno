0d84df7901e03669e2cbff6f0ec2fb9ba113defe
Moustafa K <mkm.fibertech@gmail.com>
2026-06-12 05:17:14 +0300

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -1,11 +1,37 @@
-fetch("header.html")
-.then(response => response.text())
-.then(data => {
-    document.getElementById("header-container").innerHTML = data;
-});
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
 
-fetch("footer.html")
-.then(response => response.text())
-.then(data => {
-    document.getElementById("footer-container").innerHTML = data;
-});
+        document.getElementById(
+            'header-container'
+        ).innerHTML = data;
+
+        const currentPage =
+            window.location.pathname
+            .split('/')
+            .pop() || 'index.html';
+
+        document.querySelectorAll(
+            '.main-nav a'
+        ).forEach(link => {
+
+            if (
+                link.getAttribute('href')
+                === currentPage
+            ) {
+
+                link.classList.add(
+                    'active'
+                );
+            }
+        });
+    });
+
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+
+        document.getElementById(
+            'footer-container'
+        ).innerHTML = data;
+    });
